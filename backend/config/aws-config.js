00f204e8d6f0013c076c23b8a177d2cd726c0c94
Rajeev-kushwaha-123 <rajeevkushwaha886@gmail.com>
2025-08-18 07:55:37 +0530

    all code

--- backend/config/aws-config.js
+++ backend/config/aws-config.js
@@ -1,14 +1,16 @@
+import dotenv from "dotenv";
+dotenv.config();
 
 const AWS=require('aws-sdk');
 
-AWS.config.update({region:"ap-south-1",
-    accessKeyId:"AKIAXDP3VEDIGI6PML5S",
-    secretAccessKey:"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"
+AWS.config.update({region:process.env.REGION,
+    accessKeyId:process.env.ACCESS_KEY,
+    secretAccessKey:process.env.SECRET_ACCESS_KEY
 });
 
 const s3= new AWS.S3();
 
-const S3_BUCKET="simpledemouserbucket";
+const S3_BUCKET=process.env.BUCKET_NAME;
 
 module.exports={s3,S3_BUCKET};
 
